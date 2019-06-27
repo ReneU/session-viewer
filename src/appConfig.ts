546ab87dabc76f7162d6b514234d6df460d70754
reun <r.unrau@conterra.de>
2019-06-27 10:15:31 +0200

    fix formatting and translation

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -20,30 +20,30 @@ const ui = {
     },
     descriptionTexts: {
       start: `
-              Das 'Session-Viewer' ermöglicht den interaktiven Vergleich zweier Datensätze mit Hilfe verschiedener Analyselayer.
-              Die Veränderungen an einer Ansicht werden automatisch auf die andere Ansicht synchronisiert (Ausschnitt, aktive Layer).
-              Dadurch können Analyseparameter auf beide Datensätze angewendet und verglichen werden.
-              Klicken Sie zum Starten auf einen der beiden Layer in der oberen, linken Ecke (Inhaltsbaum).
-              Sie können zwischen den "rohen" Position der "Karteninteraktionen" und den "Aggregierten Trajektorien" wählen.
-              Der dritte und letzte Layer enthät die Geometrien aus der ursprünglichen Aufgabe und kann als Referenz genutzt werden.
-          `,
+        Das 'Session-Viewer' ermöglicht den interaktiven Vergleich zweier Datensätze mit Hilfe verschiedener Analyselayer.
+        Die Veränderungen an einer Ansicht werden automatisch auf die andere Ansicht synchronisiert (Ausschnitt, aktive Layer).
+        Dadurch können Analyseparameter auf beide Datensätze angewendet und verglichen werden.
+        Klicken Sie zum Starten auf einen der beiden Layer in der oberen, linken Ecke (Inhaltsbaum).
+        Sie können zwischen den "rohen" Position der "Karteninteraktionen" und den "Aggregierten Trajektorien" wählen.
+        Der dritte und letzte Layer enthät die Geometrien aus der ursprünglichen Aufgabe und kann als Referenz genutzt werden.
+      `,
       interactionSlider: `
-              Der Layer "Karteninteraktionen" zeigt die Positionen einzelner Zoom- und Pan-Interaktionen, wobei die Farbe eine zusätzliche Metrik darstellt.
-              Diese Metrik können Sie über das Menü unterhalb des Layer-Titels auswählen ("drei Punkte").
-              Die verfügbaren Metriken sind dabei in zwei Kategorien unterteil: Effizient und Zufriedenheit.
-              Wenn nur eine Metrik aus einer Kategorie aktiviert ist, erscheint ein zusätzlicher Slider-Widget in der unteren rechten Ecke.
-              Der Slider ermöglicht eine Veränderung der Klassen für die Visualisierung der Metrik und des Modus ("Niedrige zu Hohen Werten" und "Über und unter Durchschnitt").
-              Durch die Aktivierung zweier Metriken aus unterschiedlichen Kategorien, können Sie auch deren Beziehung visualisieren.
-          `,
+        Der Layer "Karteninteraktionen" zeigt die Positionen einzelner Zoom- und Pan-Interaktionen, wobei die Farbe eine zusätzliche Metrik darstellt.
+        Diese Metrik können Sie über das Menü unterhalb des Layer-Titels auswählen ("drei Punkte").
+        Die verfügbaren Metriken sind dabei in zwei Kategorien unterteil: Effizient und Zufriedenheit.
+        Wenn nur eine Metrik aus einer Kategorie aktiviert ist, erscheint ein zusätzlicher Slider-Widget in der unteren rechten Ecke.
+        Der Slider ermöglicht eine Veränderung der Klassen für die Visualisierung der Metrik und des Modus ("Niedrige zu Hohen Werten" und "Über und unter Durchschnitt").
+        Durch die Aktivierung zweier Metriken aus unterschiedlichen Kategorien, können Sie auch deren Beziehung visualisieren.
+      `,
       interactionRelationship: `
-              Visualisierungen von Beziehungen ermöglichen die Darstellungen zweier Muster in einer Karten und zeigen, ob zwei Dinge in Beziehung stehen.
-              Das statische Legendenwidget in der unteren rechten Ecke beschreibt die Bedeutung der Farben in der Karte.
-          `,
-          aggregatedTracks: `
-              Die "Aggregierten Nutzertrajektorien" zeigen zusammengefasste WebGIS Sitzungen, wobei die Farbe die Differenz des Zoom-Level zwischen zwei Punkten darstellt.
-              Ein negativer Wert bedeutet, dass der Nutzer sich von der Kartenoberfläche entfernt hat ("herauszoomen").
-              Ein positiver Wert bedeutet, dass der Nutzer in die Karte herein gezoomt hat.
-          `
+        Visualisierungen von Beziehungen ermöglichen die Darstellungen zweier Muster in einer Karten und zeigen, ob zwei Dinge in Beziehung stehen.
+        Das statische Legendenwidget in der unteren rechten Ecke beschreibt die Bedeutung der Farben in der Karte.
+      `,
+      aggregatedTracks: `
+        Die "Aggregierten Nutzertrajektorien" zeigen zusammengefasste WebGIS Sitzungen, wobei die Farbe die Differenz des Zoom-Level zwischen zwei Punkten darstellt.
+        Ein negativer Wert bedeutet, dass der Nutzer sich von der Kartenoberfläche entfernt hat ("herauszoomen").
+        Ein positiver Wert bedeutet, dass der Nutzer in die Karte herein gezoomt hat.
+      `
     }
   },
   en: {
@@ -86,7 +86,8 @@ const ui = {
       `,
       aggregatedTracks: `
         The "Aggregated User-Trajectories" layer shows the aggregated tracks of users sessions whereas the color is used for visualizing the value of selected measures.
-        You can see and change the selected measures by opening the dot-menu next to the layer's title.
+        A negative value means, that the user zoomed out.
+        A positive value means, that the user zoomed in.
       `
     }
   }
